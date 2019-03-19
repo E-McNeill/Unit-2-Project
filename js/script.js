ad2f4d85434fc5bbdeb8e6db0ec26b78d2ea9b3d
E-McNeill <ericaksaunders@gmail.com>
2019-03-19 13:27:58 -0700

    final edits and polishing

--- js/script.js
+++ js/script.js
@@ -1,114 +1,68 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+By: Erica Saunders
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+The variables below are global variables referenced throughout the program.
 ***/
 const thelist = document.querySelectorAll('.student-item'); // Selects all the student items 
-console.log(thelist.length); // Logs the number of students 
-const pagesRequired = Math.ceil(thelist.length/10); //How many pages will go on the bottom
-console.log(pagesRequired); // Logs the number of pages
-const pages = document.links.length; // counts all the links in the page aka how many pages there are. 
-console.log(pages); // Logs the number of each page 
+const pagesRequired = Math.ceil(thelist.length/10); // How many pages will go on the bottom
 
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+The function below is designed to show only the items that the user wants to see. 10 items per page. 
 ***/
 const showPage = (list, page) => {
  const first = (page * 10) - 10;
  const last = (page * 10) -1;  
   for (let i = 0; i < list.length; i+=1) {
-           if (i >= first && i <= last ) {
-      list[i].style.display = 'block';
-    } else {
-      list[i].style.display = 'none';
+         if (i >= first && i <= last ) {
+            list[i].style.display = 'block';
+            } else {
+            list[i].style.display = 'none';
        }
+   }
 }
-}
 
-
-
+/***  
+The function below is designed to generate, append, and add functionality to the buttons on the bottom of the webpage. 
+***/ 
 
 
 const appendPageLinks = (list) => {
-//Creates the new div elements on the bottom 
-  const ul = document.querySelector('.page');  // Selects the main div
-  const paginationDiv = document.createElement('div'); // makes the new div
-  paginationDiv.className = 'pagination'; // gives the new div a class name
-  ul.append(paginationDiv); //sticks the new div after the ul
-  const pdClass = document.querySelector('.pagination'); // selects the paginaiton class(the new div)
-  const paginationList = document.createElement('ul'); //creates the ul that goes in the bottom div
-  pdClass.append(paginationList); // sticks the ul into the bottom div
-  for (let i=0; i <pagesRequired; i+=1){
-     const a = document.createElement('li'); //creates a list item
-     const link = document.createElement('a');    //creates a link item
-    link.textContent = [i+1]; // label for the list item
-    link.href = '#'
-    a.append(link); 
-    paginationList.append(a); // Sticks the button at the bottom of the main div
-  
-const buttonLinks = parseInt(a.textContent);
-
-  
-// looping test     
-    
-    const firstLinkActive = document.querySelector('a');
-   // const linkTest1 = document.querySelectorAll('a');
-    firstLinkActive.classList = ('active'); // shows the first page as active on load
-  // for (let i = 0; i < linkTest1.length; i+=1) {
-   
-      a.addEventListener('click', (e) => {    
-      showPage(thelist, buttonLinks);
+  // New div with ul is created and appended to the page.
+   const ul = document.querySelector('.page');  
+  const paginationDiv = document.createElement('div'); 
+  paginationDiv.className = 'pagination'; 
+  ul.append(paginationDiv); 
+  const paginationClass = document.querySelector('.pagination'); 
+  const paginationList = document.createElement('ul'); 
+  paginationClass.append(paginationList); 
+  // Loops over the pages required and creates a list item for each page.
+  for (let i=0; i <pagesRequired; i+=1){ 
+     const a = document.createElement('li'); 
+     const link = document.createElement('a');    
+     link.textContent = [i+1]; 
+     link.href = '#'
+     a.append(link); 
+     paginationList.append(a); 
+     const buttonLinks = parseInt(a.textContent); // Takes the page as a number to pass to the showPage function
+     // First link on the page is selected, and marked as active so the formatting appears on page load.
+     const firstLinkActive = document.querySelector('a'); 
+     firstLinkActive.classList = ('active'); 
+     // Event listener notices when a page number is clicked, and then calls the showPage function to show the correct page. 
+     a.addEventListener('click', (e) => {    
+     showPage(thelist, buttonLinks);
      const linkTest1 = document.querySelectorAll('a');   
-   for (let i = 0; i < linkTest1.length; i+=1) {
-    linkTest1[i].classList.remove('active');
-    }
+      for (let i = 0; i < linkTest1.length; i+=1) {
+         linkTest1[i].classList.remove('active');
+         }
     e.target.classList = 'active';
         });
-    }
-                                                
-                      
-//}                              
+    }                       
  }
-  
-  
-
 appendPageLinks();
+showPage(thelist, 1); // Shows first page with 10 items on page load 
 
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// /Remember to delete the comments that came with this file, and replace them with your own code comments.
